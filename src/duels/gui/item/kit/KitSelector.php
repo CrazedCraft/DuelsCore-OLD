b8d3ba8c99b6bdd724f262e76ba831fd0b128a50
JackNoordhuis <daswaggamer@gmail.com>
2018-02-14 18:02:15 +1100

    Uncomment some debug code

--- src/duels/gui/item/kit/KitSelector.php
+++ src/duels/gui/item/kit/KitSelector.php
@@ -36,11 +36,11 @@ class KitSelector extends GUIItem {
 			return;
 		}
 		if(!$session->inParty()) {
-			//if($player->getPlayerProtocol() >= Info::PROTOCOL_120) {
-			//	$player->showModal($player->getCore()->getUIManager()->getForm(PlayDuelTypeSelectionForm::FORM_UI_ID));
-			//} else {
+			if($player->getPlayerProtocol() >= Info::PROTOCOL_120) {
+				$player->showModal($player->getCore()->getUIManager()->getForm(PlayDuelTypeSelectionForm::FORM_UI_ID));
+			} else {
 				$player->openGuiContainer($player->getCore()->getGuiManager()->getContainer(PlayDuelTypeSelectionContainer::CONTAINER_ID));
-			//}
+			}
 		} else {
 			$player->sendMessage(TF::RED . "You must use the party event item or the NPC's to play duels while in a party!");
 		}
